219a0ad39d4e8c3efd2a5b0bcbdde39fc75fd88c
und3rdg <und3rdg@gmail.com>
2019-08-28 22:47:50 +0100

    Created all div's, fix clearing bug in update method

--- src/js/PlayerCard.js
+++ src/js/PlayerCard.js
@@ -2,49 +2,74 @@ import Api from './Api'
 
 export default class {
     constructor({dom_id, dom_className, dom_dataAttr}) {
+        this.dom = {
+            root: null,
+            // cards: [], I have a feeling that this array can be used for grid of cards
+            card: document.createElement('div'),
+            dropdown: document.createElement('div'),
+            img: document.createElement('div'),
+            logo: document.createElement('div'),
+            title: document.createElement('div'),
+            subtitle: document.createElement('div'),
+            stats: document.createElement('div'),
+        }
+
+
+        this.old_dom = {} // helper obj, history buffer
+
         this.dom_root
         this.data
-        this.old_card
 
         if(!!dom_id) {
-            this.dom_root = document.getElementById(dom_id)
+            this.dom.root = document.getElementById(dom_id)
         } else if (!!dom_dataAttr) {
-            this.dom_root = document.querySelector('[data-' + dom_dataAttr + ']')
+            this.dom.root = document.querySelector('[data-' + dom_dataAttr + ']')
         } else if (!!dom_className) {
-            this.dom_root = document.querySelector('.' + dom_className)
+            this.dom.root = document.querySelector('.' + dom_className)
         } 
-        this.api_url = this.dom_root.dataset.url
-        this.url = this.api_url
 
 
+        this.api_url = this.dom.root.dataset.url
         this.api = new Api({url: this.api_url, test: true})
-        this.api.get_data({cb: this.update.bind(this), test: true})
+        this.api.get_data({cb: this.update.bind(this)})
+
+        this.dom = this.set_classNames(this.dom)
     }
 
     card() {
         if(!this.data) {
             return null
         }
+        const {card} = this.dom
         
-        const div = document.createElement('div')
-        div.classList.add('card')
+        card.classList.add('card')
 
         // tmp
         const pre = document.createElement('pre')
         pre.innerText = this.data && JSON.stringify(this.data, null, 4)
-        div.append(pre)
+        card.append(pre)
 
-        return div
+        return card
     }
 
     update(data) {
         this.data = data
 
-        if(!!this.old_card) {
-            this.dom_root.removeChild(this.old_card)
-        }
-        this.dom_root.append(this.card())
-        this.old_card = this.card()
+        this.dom.root.innerHTML = "" // 😥
+
+        this.dom.card = this.card()
+        this.dom.root.append(this.dom.card)
+    }
+
+    dropdown() {
+    }
+
+    set_classNames(dom) {
+        Object.keys(dom).forEach(key=>dom[key].classList.add(key))
+        return dom
     }
 
 }
+
+
+
--- test/PlayerCard.test.js
+++ test/PlayerCard.test.js
@@ -35,34 +35,84 @@ describe('PlayerCard.js', () => {
 
         it('should have api key', () => {
             const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playerCard.api.url).to.not.be.empty // BTW it should not be hardcoded, sry
+            expect(playerCard.api.url).to.not.be.empty
         })
     })
 
 
     describe('Creating card on initialization', () => {
-        it('should have dom_root key', () => {
+        it('should have dom key', () => {
             const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playerCard).to.have.any.key('dom_root')
+            expect(playerCard).to.have.any.key('dom')
         })
 
-        it('should have dom_root key', () => {
+        it('should have api key', () => {
             const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard).to.have.any.key('api')
         })
 
         it('should create div with class .card', () => {
             const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playerCard.dom_root.firstElementChild.classList.contains('card')).to.be.ok
+            expect(playerCard.dom.root.firstElementChild.classList.contains('card')).to.be.ok
+        })
+
+        it("should clean old div's and always have only one child", () => {
+            const playerCard = new PlayerCard({dom_id: "id"})
+            expect(playerCard.dom.root.childElementCount).to.eq(1)
+            //////////////////////////////////////// //////////////////////////////////////
+            // expect(playerCard.dom.root).to.eq('')
         })
     })
 
     // from now on TDD 
-    describe('dom elements', () => {
-        it('should have data-url attr', () => {
+    describe('dom elements', () => { // 😥
+        it('should have link from data-url attr', () => {
             const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.api_url).to.eq(api_url)
-        } )
+
+        })
+
+        it('should have old_dom that is and object', () => {
+            const playerCard = new PlayerCard({dom_id: "id"})
+            expect(playerCard.old_dom).to.be.an('object')
+        })
+
+        const divs = [ 'root', 'card', 'dropdown', 'img', 'logo', 'title', 'subtitle', 'stats']
+
+        divs.forEach(div => {
+            it(`should have dom object with key [ ${div} ]`, () => {
+                const playerCard = new PlayerCard({dom_id: "id"})
+                expect(playerCard.dom).to.be.an('object')
+                    .that.have.any.key(div)
+            })
+            it(`should be div [ ${div} ]`, () => {
+                const playerCard = new PlayerCard({dom_id: "id"})
+                expect(playerCard.dom[div].tagName).to.eq('DIV')
+            })
+            it(`should have a class [ ${div} ]`, () => {
+                const playerCard = new PlayerCard({dom_id: "id"})
+                expect(playerCard.dom[div].classList[0]).to.eq(div)
+            })
+        })
     })
 
+
+
+    describe('pure functions', () => { // 🙃 
+        const dom = {
+            wrap: document.createElement('div'),
+            footer: document.createElement('div'),
+        }
+        it('should return object with same keys', () => {
+            const playerCard = new PlayerCard({dom_id: "id"})
+            expect(playerCard.set_classNames(dom)).to.have.keys('wrap', 'footer')
+        })
+        it('should return object and add className to each element', () => {
+            const playerCard = new PlayerCard({dom_id: "id"})
+            expect(playerCard.set_classNames(dom).wrap.classList.value).to.eq('wrap')
+            expect(playerCard.set_classNames(dom).footer.classList.value).to.eq('footer')
+        })
+    })
 })
+
+
