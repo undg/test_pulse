7635429411c5ee2be9a2565af789f76938632f03
und3rdg <und3rdg@gmail.com>
2019-08-31 00:34:56 +0100

    DRY test file

--- test/PlayerCard.test.js
+++ test/PlayerCard.test.js
@@ -12,29 +12,27 @@ const json = require('../api/player-stats.json')
 import PlayerCard from './../src/js/PlayerCard'
 
 describe('PlayerCard.js', () => {
-    beforeEach(function () {
-        moxios.install()
-        moxios.stubRequest(api_url, {
-            status: 200,
-            response: {
-                data: json,
-            },
-        })
+    moxios.install()
+    moxios.stubRequest(api_url, {
+        status: 200,
+        response: {
+            data: json,
+        },
     })
 
-    afterEach(function () {
+    after(function () {
         moxios.uninstall()
     })
 
 
-    describe('api', () => {
+    const playerCard = new PlayerCard({dom_id: "id"})
+
+    describe('constructor', () => {
         it('should have api key', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard).to.have.any.key('api')
         })
 
         it('should have api key', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.api.url).to.not.be.empty
         })
     })
@@ -42,36 +40,17 @@ describe('PlayerCard.js', () => {
 
     describe('Creating card on initialization', () => {
         it('should have dom key', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard).to.have.any.key('dom')
-        })
-
-        it('should have api key', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playerCard).to.have.any.key('api')
-        })
-
-        it.skip('should create div with class .card', () => { // 😥
-            const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playerCard.dom.root.firstElementChild.classList.contains('card')).to.be.ok
-        })
-
-        it.skip("should clean old div's and always have only one child", () => { // 😥
-            const playerCard = new PlayerCard({dom_id: "id"})
-            expect(playercard.dom.root.childelementcount).to.eq(1)
         })
     })
 
     // from now on TDD  // 🙃 
     describe('dom elements', () => {
         it('should have link from data-url attr', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.api_url).to.eq(api_url)
-
         })
 
         it('should have old_dom that is and object', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.old_dom).to.be.an('object')
         })
 
@@ -79,16 +58,13 @@ describe('PlayerCard.js', () => {
 
         divs.forEach(div => {
             it(`should have dom object with key [ ${div} ]`, () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 expect(playerCard.dom).to.be.an('object')
                     .that.have.any.key(div)
             })
             it(`should be div [ ${div} ]`, () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 expect(playerCard.dom[div].tagName).to.eq('DIV')
             })
             it(`should have a class [ ${div} ]`, () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 expect(playerCard.dom[div].classList[0]).to.eq(div)
             })
         })
@@ -103,18 +79,15 @@ describe('PlayerCard.js', () => {
         }
 
         it('should return object with same keys', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.set_classNames(dom)).to.have.keys('wrap', 'footer')
         })
 
         it('should return object and add className to each element', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.set_classNames(dom).wrap.classList.value).to.eq('wrap')
             expect(playerCard.set_classNames(dom).footer.classList.value).to.eq('footer')
         })
 
         it('should be and function', () => {
-            const playerCard = new PlayerCard({dom_id: "id"})
             expect(playerCard.card).to.be.an('Function')
         })
 
@@ -122,37 +95,31 @@ describe('PlayerCard.js', () => {
 
         describe('card()', () => {
             it('should append div.dropdown', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[0].classList[0]).to.eq('dropdown')
             })
 
             it('should append div.img', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[1].classList[0]).to.eq('img')
             })
 
             it('should append div.logo', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[2].classList[0]).to.eq('logo')
             })
 
             it('should append div.title', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[3].classList[0]).to.eq('title')
             })
 
             it('should append div.subtitle', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[4].classList[0]).to.eq('subtitle')
             })
 
             it('should append div.stats', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const card = playerCard.card({dom: playerCard.dom, data: json})
                 expect(card.childNodes[5].classList[0]).to.eq('stats')
             })
@@ -162,37 +129,26 @@ describe('PlayerCard.js', () => {
 
         describe('stats()', () => {
             it('should be OK', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
-                const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
-                expect(stats).to.be.ok
-            })
-
-            it('should be OK', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
                 expect(stats.childElementCount).to.eq(5)
             })
 
             it('should have grandchild <p>', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
                 expect(stats.firstElementChild.childNodes[0].tagName).to.eq('P')
             })
 
             it('should have text "Appearances"', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
                 expect(stats.firstElementChild.childNodes[0].innerText).to.eq('Appearances')
             })
             it('should have another grandchild <p>', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
                 expect(stats.firstElementChild.childNodes[1].tagName).to.eq('P')
             })
             it('should have score as a string(80)', () => {
-                const playerCard = new PlayerCard({dom_id: "id"})
                 const stats = playerCard.stats({stats_dom: playerCard.dom.stats, stats_data: json.players[0].stats})
-                expect(stats.firstElementChild.childNodes[1].innerText).to.an('string').that.is.eq('80')
+                expect(stats.firstElementChild.childNodes[1].innerText).to.be.an('string').that.is.eq('80')
             })
         })
 
