c39d1fd56b6516533ce0e2389134fee22065132f
und3rdg <und3rdg@gmail.com>
2019-08-31 22:24:09 +0100

    Pre publish to npm

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import PlayerCard from "./PlayerCard"
 // few other options
 // const playerCard = new PlayerCard({dom_id: "StatCard"})
 // const playerCard = new PlayerCard({dom_className: "StatCard"})
-
-document.addEventListener("DOMContentLoaded", () => {
-    const playerCard = new PlayerCard({dom_dataAttr: "StatCard", document: document})
-})
+export default function init(){
+    document.addEventListener("DOMContentLoaded", () => {
+        const playerCard = new PlayerCard({dom_dataAttr: "StatCard", document: document})
+    })
+}
+init()
