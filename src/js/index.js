74797a8da4841c80e8c03e61b2248c724891d2c7
und3rdg <und3rdg@gmail.com>
2019-08-31 23:02:00 +0100

    README: More details about instalation proccess and .npmignore to publish dist folder.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import PlayerCard from "./PlayerCard"
-
-// few other options
-// const playerCard = new PlayerCard({dom_id: "StatCard"})
-// const playerCard = new PlayerCard({dom_className: "StatCard"})
 export default function init(){
     document.addEventListener("DOMContentLoaded", () => {
-        const playerCard = new PlayerCard({dom_dataAttr: "StatCard", document: document})
+        const playerCard = new PlayerCard({dom_dataAttr: "StatCard"})
     })
 }
 init()
+
+// few other options
+//
+// const playerCard = new PlayerCard({dom_id: "StatCard"})
+// const playerCard = new PlayerCard({dom_className: "StatCard"})
+//
+// import  '@undg/player-card'
